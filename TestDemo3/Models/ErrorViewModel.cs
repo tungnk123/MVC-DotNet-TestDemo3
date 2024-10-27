c3348347c81b5357d68130701af65b8d1a8fbd14
tungnk123 <doanthanhtungnk123@gmail.com>
2024-10-27 09:10:39 +0700

    Add project files.

---
+++ TestDemo3/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TestDemo3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
